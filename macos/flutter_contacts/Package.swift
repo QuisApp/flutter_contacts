50385dbf5c5772633c0e9bd9f395a9874d9967b4
Joachim Valente <joachim@quis.co>
2026-03-04 18:55:43 -0800

    format

--- macos/flutter_contacts/Package.swift
+++ macos/flutter_contacts/Package.swift
@@ -5,23 +5,23 @@ import PackageDescription
 let package = Package(
     name: "flutter_contacts",
     platforms: [
-        .macOS("10.15")
+        .macOS("10.15"),
     ],
     products: [
-        .library(name: "flutter-contacts", targets: ["flutter_contacts"])
+        .library(name: "flutter-contacts", targets: ["flutter_contacts"]),
     ],
     dependencies: [
-        .package(name: "FlutterFramework", path: "../FlutterFramework")
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
     ],
     targets: [
         .target(
             name: "flutter_contacts",
             dependencies: [
-                .product(name: "FlutterFramework", package: "FlutterFramework")
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
             ],
             resources: [
-                .process("PrivacyInfo.xcprivacy")
+                .process("PrivacyInfo.xcprivacy"),
             ]
-        )
+        ),
     ]
 )
